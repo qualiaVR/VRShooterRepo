a4cce7bd46441d12e8111cf248944262fb3c0946
tonyvaldes <tonyvaldes.231@gmail.com>
2019-09-17 15:32:36 -0600

    second commit

---
+++ Assets/ReloadController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReloadController : MonoBehaviour {
+
+    public bool canReload = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Gun")
+            canReload = true;
+        
+            
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Gun")
+            canReload = false;
+    }
+
+}
